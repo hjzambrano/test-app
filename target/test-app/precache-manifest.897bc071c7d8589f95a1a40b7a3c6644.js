47173af3a293b70378ecbbb618279665b6219acd
Hector Zambrano <hzambrano.ve@gmail.com>
2019-02-10 17:13:35 -0400

    App de prueba en React con war para desplegar en Wildfly

---
+++ target/test-app/precache-manifest.897bc071c7d8589f95a1a40b7a3c6644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "http://isclp-106:8080/test-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dd6c25166c70ad058ae8",
+    "url": "http://isclp-106:8080/test-app/static/js/runtime~main.dd6c2516.js"
+  },
+  {
+    "revision": "3b6a7492763affce9434",
+    "url": "http://isclp-106:8080/test-app/static/js/main.3b6a7492.chunk.js"
+  },
+  {
+    "revision": "a0c2eb18c8a18f9e76f7",
+    "url": "http://isclp-106:8080/test-app/static/js/1.a0c2eb18.chunk.js"
+  },
+  {
+    "revision": "3b6a7492763affce9434",
+    "url": "http://isclp-106:8080/test-app/static/css/main.11941e9c.chunk.css"
+  },
+  {
+    "revision": "4abb83a7f494c1a396c48c7ef35709c2",
+    "url": "http://isclp-106:8080/test-app/index.html"
+  }
+];
